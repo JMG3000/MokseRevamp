63f08896ac6b2ef4e7ce6f71a64b9ec97ec062dc
JMG3000 <Jakeincbusinesssolutions@gmail.com>
2026-06-20 23:04:42 -0500

    Configure Sentry observability

--- components/tooling/google-tag.tsx
+++ components/tooling/google-tag.tsx
@@ -1,20 +1,22 @@
+import Script from "next/script";
+
 const GOOGLE_TAG_ID = "G-VPMHQFDVV7";
 
 export function GoogleTag() {
   return (
     <>
-      <script
-        async
+      <Script
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`}
       />
-      <script
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GOOGLE_TAG_ID}');
-          `,
+          __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GOOGLE_TAG_ID}');`,
         }}
       />
     </>
